bd939aac7136e5333887ec53d303dd88c1a440af
EC2 Default User <ec2-user@ip-172-31-0-114.ap-northeast-1.compute.internal>
2024-09-25 01:52:49 +0000

    いいねコントローラー

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,15 @@
+class FavoritesController < ApplicationController
+  
+  def create
+    @train = Train.find(params[:train_id])
+    favorite = current_user.favorites.new(train_id: @train.id)
+    favorite.save
+  end
+
+  def destroy
+    @train = Train.find(params[:train_id])
+    favorite = current_user.favorites.find_by(train_id: @train.id)
+    favorite.destroy
+  end
+  
+end
