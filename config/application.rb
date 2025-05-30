e82d27520bd76ed10a0cedd62c68222c1947c1e8
EC2 Default User <ec2-user@ip-172-31-0-114.ap-northeast-1.compute.internal>
2025-05-30 14:18:32 +0000

    config.time_zone = 'Tokyo'を記載。

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module T
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+     # 日本時間に設定
+    config.time_zone = 'Tokyo'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
